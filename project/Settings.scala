fc505f4fbf57324288177657bfc113b884c19931
Jed Wesley-Smith <jed@atlassian.com>
2014-09-22 12:13:15 +1000

    upped to latest version of scala

--- project/Settings.scala
+++ project/Settings.scala
@@ -6,6 +6,9 @@ object Settings {
 
   val testSettings = testOptions in Test += Tests.Argument("console", "junitxml")
 
+  val scala210 = "2.10.4"
+  val scala211 = "2.11.2"
+
   lazy val standardSettings =
     Defaults.coreDefaultSettings ++
       testSettings ++
@@ -13,9 +16,22 @@ object Settings {
       net.virtualvoid.sbt.graph.Plugin.graphSettings ++ // dependency plugin settings
       Seq[Def.Setting[_]] (
         organization := "io.atlassian.aws-scala"
-      , scalaVersion := "2.11.1"
-      , crossScalaVersions := Seq("2.10.4", "2.11.1")
-      , scalacOptions := Seq("-deprecation", "-unchecked", "-feature", "-language:_", "-Xfatal-warnings", "-Xlog-free-terms", "-target:jvm-1.6", "-Xlint", "-Yno-adapted-args", "-Ywarn-dead-code", "-Ywarn-numeric-widen", "-Ywarn-value-discard")
+      , scalaVersion := scala211
+      , crossScalaVersions := Seq(scala210, scala211)
+      , scalacOptions := Seq(
+          "-deprecation"
+        , "-unchecked"
+        , "-feature"
+        , "-language:_"
+        , "-Xfatal-warnings"
+        , "-Xlog-free-terms"
+        , "-target:jvm-1.6"
+        , "-Xlint"
+        , "-Yno-adapted-args"
+        , "-Ywarn-dead-code"
+        , "-Ywarn-numeric-widen"
+        , "-Ywarn-value-discard"
+      )
       , javacOptions ++= Seq("-encoding", "UTF-8", "-source", "1.6", "-target", "1.6")
       , javacOptions in doc := Seq("-encoding", "UTF-8")
       , resolvers ++= Seq(
@@ -24,7 +40,7 @@ object Settings {
         , "atlassian-public"   at "https://maven.atlassian.com/content/groups/atlassian-public/"
         , "atlassian-internal" at "https://maven.atlassian.com/content/groups/internal/"
         , "Tools Releases"     at "http://oss.sonatype.org/content/repositories/releases"
-        , "Tools Snapshots"      at "http://oss.sonatype.org/content/repositories/snapshots"
+        , "Tools Snapshots"    at "http://oss.sonatype.org/content/repositories/snapshots"
         )
       , credentials += Credentials(Path.userHome / ".ivy2" / ".credentials")
       , mappings in (Compile, packageBin) ++= Seq(
