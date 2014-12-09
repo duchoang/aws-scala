966c81e3735d20ce770135b27c246cd39fbf13ef
Jed Wesley-Smith <jed@atlassian.com>
2014-12-09 17:36:56 +1100

    AwsAction._ is imported, use it

--- core/src/test/scala/io/atlassian/aws/AwsActionSpec.scala
+++ core/src/test/scala/io/atlassian/aws/AwsActionSpec.scala
@@ -22,32 +22,38 @@ class AwsActionSpec extends SpecificationWithJUnit with ScalaCheck {
 
   def withClientThrowsIsHandled =
     Prop.forAll { msg: String =>
-      AwsAction.withClient[String, String] {
+      withClient[String, String] {
         s => throw new RuntimeException(s)
-      }.run(msg).run should be like { case -\/(Invalid.Err(t)) => t.getMessage === msg }
+      }.run(msg).run should be like {
+        case -\/(Invalid.Err(t)) => t.getMessage === msg
+      }
     }
 
   def recover =
     Prop.forAll { msg: String =>
-      AwsAction.fail[Int, String](msg).recover {
+      fail[Int, String](msg).recover {
         case Invalid.Message(s) => AwsAction.ok[Int, String](s)
-        case _                  => AwsAction.fail[Int, String](msg)
-      }.run(1).run should be like { case \/-(s) => s === msg }
+        case _                  => fail[Int, String](msg)
+      }.run(1).run should be like {
+        case \/-(s) => s === msg
+      }
     }
 
   def handle =
     Prop.forAll { msg: String =>
-      AwsAction.fail[Int, String](msg).handle {
+      fail[Int, String](msg).handle {
         case Invalid.Message(s) => AwsAction.ok[Int, String](s)
-      }.run(1).run should be like { case \/-(s) => s === msg }
+      }.run(1).run should be like {
+        case \/-(s) => s === msg
+      }
     }
 
   def amazonNotFound =
     Prop.forAll { msg: String =>
-      AwsAction.withClient[String, String] {
+      withClient[String, String] {
         s => throw new AmazonServiceException(s) <| { _.setStatusCode(404) }
       }.run(msg).run should be like {
-        case -\/(Invalid.Err(AmazonExceptions.ServiceException(ExceptionType.NotFound, t))) => t.getErrorMessage === msg
+        case -\/(Invalid.Err(ServiceException(ExceptionType.NotFound, t))) => t.getErrorMessage === msg
       }
     }
 }
