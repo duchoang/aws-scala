e8a0924c3f2193d0c8147c076b2823db41298c86
Ashley Valent <avalent@atlassian.com>
2015-03-26 15:11:24 +1100

    Added rds module to Modules

---
+++ core/src/main/scala/io/atlassian/aws/AvailabilityZone.scala
@@ -0,0 +1,18 @@
+package io.atlassian.aws
+
+sealed trait AvailabilityZone {
+  import AvailabilityZone._
+
+  def fold[X](multi: => X, zone: String => X): X =
+    this match {
+      case Multiple => multi
+      case Zone(n) => zone(n)
+    }
+}
+
+object AvailabilityZone {
+  case object Multiple extends AvailabilityZone
+
+  // TODO - the zone should be an enum really
+  case class Zone(name: String) extends AvailabilityZone
+}
