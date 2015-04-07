2d27e11752b2cd4bf59f15dae6b185e136c2e3c4
Sidney Shek <sshek@atlassian.com>
2015-04-07 14:27:59 +1000

    Reduce number of parameters so scala 2.10 compiler doesn't complain.

--- rds/src/main/scala/io/atlassian/aws/rds/CreateInstance.scala
+++ rds/src/main/scala/io/atlassian/aws/rds/CreateInstance.scala
@@ -6,60 +6,63 @@ import scala.collection.JavaConverters._
 import scala.concurrent.duration.Duration
 import scalaz.syntax.id._
 
+case class BackupParameters(backupRetentionPeriod: Option[Duration] = None,
+                            backupWindow: Option[String] = None) // TODO - Probably should be duration that formats as expected
+
+case class SecurityParameters(securityGroupName: List[String] = Nil,
+                              publiclyAccessible: Boolean = true,
+                              storageEncrypted: Boolean = false,
+                              kmsKeyId: Option[String] = None,
+                              tdeCredentialArn: Option[String] = None,
+                              tdeCredentialPassword: Option[String] = None,
+                              vpcSecurityGroupId: List[String] = Nil)
 case class CreateInstance(engine: Engine,
                           id: DbId,
                           allocatedStorage: Int,
                           instanceClass: DbInstanceClass,
                           masterUsername: String,
                           masterPassword: String,
+                          backupParameters: BackupParameters,
+                          securityParameters: SecurityParameters,
                           availabilityZone: AvailabilityZone = AvailabilityZone.Multiple,
-                          backupRetentionPeriod: Option[Duration] = None,
                           characterSetName: Option[String] = None,
                           optionGroupName: Option[String] = None,
                           dbParameterGroupName: Option[String] = None,
-                          securityGroupName: List[String] = Nil,
                           subnetGroupName: Option[String] = None,
                           iops: Option[Int] = None,
-                          kmsKeyId: Option[String] = None,
                           licenceModel: Option[LicenceModel] = None,
                           dbName: Option[String] = None,
                           autoMinorVersionUpgrade: Boolean = true,
-                          publiclyAccessible: Boolean = true,
-                          backupWindow: Option[String] = None, // TODO - Probably should be duration that formats as expected
                           maintenanceWindow: Option[String] = None,
                           port: Option[Int] = None,
-                          storageEncrypted: Boolean = false,
                           storageType: Option[StorageType] = None,
-                          tags: Map[String, String] = Map.empty,
-                          tdeCredentialArn: Option[String] = None,
-                          tdeCredentialPassword: Option[String] = None,
-                          vpcSecurityGroupId: List[String] = Nil) {
+                          tags: Map[String, String] = Map.empty) {
   def aws: CreateDBInstanceRequest = {
     val req = new CreateDBInstanceRequest()
       .withDBInstanceIdentifier(id.unwrap)
       .withAllocatedStorage(allocatedStorage)
       .withAutoMinorVersionUpgrade(autoMinorVersionUpgrade)
-      .withPubliclyAccessible(publiclyAccessible)
-      .withStorageEncrypted(storageEncrypted)
-      .withVpcSecurityGroupIds(vpcSecurityGroupId.asJava)
+      .withPubliclyAccessible(securityParameters.publiclyAccessible)
+      .withStorageEncrypted(securityParameters.storageEncrypted)
+      .withVpcSecurityGroupIds(securityParameters.vpcSecurityGroupId.asJava)
       .withTags(tags.map { case (k, v) => new Tag().withKey(k).withValue(v) }.asJavaCollection)
-      .withDBSecurityGroups(securityGroupName.asJava) |> availabilityZone.aws
+      .withDBSecurityGroups(securityParameters.securityGroupName.asJava) |> availabilityZone.aws
 
-    backupRetentionPeriod.foreach { d => req.setBackupRetentionPeriod(d.toDays.toInt) }
+    backupParameters.backupRetentionPeriod.foreach { d => req.setBackupRetentionPeriod(d.toDays.toInt) }
     characterSetName.foreach { req.setCharacterSetName }
     optionGroupName.foreach { req.setOptionGroupName }
     dbParameterGroupName.foreach { req.setDBParameterGroupName }
     subnetGroupName.foreach { req.setDBSubnetGroupName }
     iops.foreach { i => req.setIops(Integer.valueOf(i)) }
-    kmsKeyId.foreach { req.setKmsKeyId }
+    securityParameters.kmsKeyId.foreach { req.setKmsKeyId }
     licenceModel.foreach { l => req.setLicenseModel(l.name) }
     dbName.foreach { req.setDBName }
-    backupWindow.foreach { req.setPreferredBackupWindow }
+    backupParameters.backupWindow.foreach { req.setPreferredBackupWindow }
     maintenanceWindow.foreach { req.setPreferredMaintenanceWindow }
     port.foreach { p => req.setPort(Integer.valueOf(p)) }
     storageType.foreach { s => req.setStorageType(s.name) }
-    tdeCredentialArn.foreach { req.setTdeCredentialArn }
-    tdeCredentialPassword.foreach { req.setTdeCredentialPassword }
+    securityParameters.tdeCredentialArn.foreach { req.setTdeCredentialArn }
+    securityParameters.tdeCredentialPassword.foreach { req.setTdeCredentialPassword }
 
     req
   }
