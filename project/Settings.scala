d3766349635c6b8b8ac7e10bc830196a968a320e
Sidney Shek <sshek@atlassian.com>
2015-07-23 23:27:00 +1000

    Restore resolvers

--- project/Settings.scala
+++ project/Settings.scala
@@ -47,6 +47,11 @@ object Settings {
           .setPreference(AlignSingleLineCaseStatements, true)
           .setPreference(AlignParameters, true)
       , addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.6.3")
+      , resolvers ++= Seq(
+          Resolver.sonatypeRepo("releases"),
+          Resolver.sonatypeRepo("snapshots"),
+          Resolver.typesafeIvyRepo("releases")
+        )
       , licenses := Seq("Apache2" -> url("https://bitbucket.org/atlassian/aws-scala/raw/master/LICENSE"))
       , homepage := Some(url("https://bitbucket.org/atlassian/aws-scala"))
       , pomExtra := (
