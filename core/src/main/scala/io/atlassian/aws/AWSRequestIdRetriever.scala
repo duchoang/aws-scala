838c01e775c55d1324d246dab1fe645c1a80e974
Jed Wesley-Smith <jed@atlassian.com>
2015-07-28 18:02:10 +1000

    refactored to be a bit more explicit about being passed around on the stack, and to prevent multi-threaded calling with a thread-id filter

--- core/src/main/scala/io/atlassian/aws/AWSRequestIdRetriever.scala
+++ core/src/main/scala/io/atlassian/aws/AWSRequestIdRetriever.scala
@@ -6,6 +6,9 @@ import com.amazonaws._
 import kadai.Invalid
 
 import scalaz.Monoid
+import scalaz.syntax.id._
+import scalaz.syntax.monad._
+import scalaz.std.option._
 import scalaz.syntax.std.option._
 import scala.collection.convert.decorateAsScala._
 
@@ -13,63 +16,53 @@ case class HttpHeaders(headers: Map[String, String])
 
 object AWSRequestIdRetriever {
 
-  private class RequestHandler[W](c: Any,
-                                  fromHeaders: Option[HttpHeaders => Option[W]],
-                                  fromException: Option[AmazonServiceException => Option[W]])
-      extends RequestHandler2 {
-    private type MutableHeaders = java.util.Map[String, String]
-    private val client = c match {
-      case client: AmazonWebServiceClient => Some(client)
-      case _                              => None
+  // [A] must be whatever the aws client returns
+  def withClient[C, W, A](f: C => A)(fromHeaders: Option[HttpHeaders => Option[W]], fromException: Option[AmazonServiceException => Option[W]])(implicit M: AwsActionMonad[C, W], wmonoid: Monoid[W]): AwsAction[C, W, A] = {
+    // holds the headers reference, lives for the lifetime of the call only
+    class Handler extends RequestHandler2 {
+      def execute(c: C): Attempt[(W, A)] = {
+        val client = ConcreteClient.unapply(c)
+        client.foreach { _.addRequestHandler(this) }
+        try
+          Attempt.safe(f(c)).map { a => (metaData, a) }
+        finally
+          client.foreach { _.removeRequestHandler(this) }
+      }
+
+      val tid = Thread.currentThread.getId
+      var headers: Option[HttpHeaders] = none
+
+      def metaData: W =
+        ~(headers <*> fromHeaders).join
+
+      override def afterResponse(request: Request[_], response: Response[_]): Unit =
+        if (Thread.currentThread.getId == tid) // prevent multiple request threads stomping on our data
+          headers = HttpHeaders(response.getHttpResponse.getHeaders.asScala.toMap).some
+
+      override def afterError(request: Request[_], response: Response[_], e: Exception) = ()
+      override def beforeRequest(request: Request[_]) = ()
     }
-    private val holder = new ThreadLocal[MutableHeaders]()
-    override def afterError(request: Request[_], response: Response[_], e: Exception) = ()
-    override def beforeRequest(request: Request[_]) = ()
 
-    override def afterResponse(request: Request[_], response: Response[_]) = {
-      holder.set(response.getHttpResponse.getHeaders)
-      ()
+    def exceptionMetadata(ex: AmazonServiceException): W =
+      ~{ fromException >>= { _(ex) } }
+
+    M.ask >>= { c =>
+      new Handler().execute(c).fold({
+        // extract the useful info
+        case Invalid.Err(ex: AmazonServiceException) =>
+          M.tell(exceptionMetadata(ex)) >> M.raiseError[A](Invalid.Err(AmazonExceptions.ServiceException.from(ex).getOrElse(ex)))
+        case i => M.raiseError(i)
+      }, {
+        case (metadata, a) => M.tell(metadata) >> M.point(a) // write the metadata
+      })
     }
-
-    private val httpHeaders: MutableHeaders => HttpHeaders =
-      res => HttpHeaders(res.asScala.toMap)
-
-    def addMe(): Unit = client.foreach(_.addRequestHandler(this))
-    def removeMe(): Unit = client.foreach(_.removeRequestHandler(this))
-
-    def buildMetaData(implicit M: Monoid[W]): W =
-      ~(for {
-        v <- Option(holder.get())
-        f <- fromHeaders
-        w <- f(httpHeaders(v))
-      } yield w)
-
-    def buildMetaDataFromException(ase: AmazonServiceException)(implicit M: Monoid[W]): W =
-      ~(for {
-        f <- fromException
-        w <- f(ase)
-      } yield w)
   }
 
-  // A must be whatever the aws client returns
-  def withClient[C, W, A](f: C => A)(fromHeaders: Option[HttpHeaders => Option[W]], fromException: Option[AmazonServiceException => Option[W]])(implicit monad: AwsActionMonad[C, W], wmonoid: Monoid[W]): AwsAction[C, W, A] = {
-    import monad.monadSyntax._
-
-    monad.ask >>= {
-      c =>
-        val handler = new RequestHandler(c, fromHeaders, fromException)
-        handler.addMe()
-        val result = Attempt.safe(f(c))
-        handler.removeMe()
-
-        result.fold({
-          case Invalid.Err(ase: AmazonServiceException) =>
-            monad.tell(handler.buildMetaDataFromException(ase)) >>= { _ => monad.raiseError[A](Invalid.Err(AmazonExceptions.ServiceException.from(ase).getOrElse(ase))) }
-          case i =>
-            monad.raiseError(i)
-        }, a => {
-          monad.tell(handler.buildMetaData) >>= { _ => monad.point(a) }
-        })
-    }
+  object ConcreteClient {
+    def unapply(c: Any): Option[AmazonWebServiceClient] =
+      c match {
+        case client: AmazonWebServiceClient => Some(client)
+        case _ => None
+      }
   }
 }
