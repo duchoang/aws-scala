6270190f02eb63225dbb516c0ca2e65810f69334
Sidney Shek <sshek@atlassian.com>
2016-09-08 20:30:58 +1000

    Remove commented lines

--- sns/src/main/scala/io/atlassian/aws/sns/package.scala
+++ sns/src/main/scala/io/atlassian/aws/sns/package.scala
@@ -1,12 +1,8 @@
 package io.atlassian.aws
 
 import com.amazonaws.services.sns.AmazonSNS
-//import com.amazonaws.services.sqs.model.MessageAttributeValue
 
 package object sns extends sns.Types {
   type SNSAction[A] = AwsAction[AmazonSNS, MetaData, A]
-
-//  type Value = Option[MessageAttributeValue]
-//  type KeyValue = Map[String, Value]
 }
 
